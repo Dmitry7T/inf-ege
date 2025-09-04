57eb5a18a23db5d16d218162c823002713fd5324
R44 <106442373+Dmitry7T@users.noreply.github.com>
2025-09-04 20:22:35 +0300

    first commit

---
+++ 1.py
@@ -0,0 +1,12 @@
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+from itertools import*
+
+tab = '24 146 56 1267 36 23457 46'.split() 
+pic = 'аб ав бв вг вд ве де ге ек кг'.split() 
+print(*range(1, 8)) 
+for var in permutations('абвгдек'): 
+    if all(str(var.index(x) + 1) in tab[var.index(y)] for x, y in pic):
+        print(*var)
